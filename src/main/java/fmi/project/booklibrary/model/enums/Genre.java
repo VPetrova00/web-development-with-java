862df54c0af4072588c98f60cb8d1ba51f6400b1
Vesela Petrova <petrovavesela00@gmail.com>
2022-05-21 03:27:33 +0300

    added annotations to the Book and Author model
    - updated application.properties file
    - added new dependency
    - added annotations to the database models Author and Book

---
+++ src/main/java/fmi/project/booklibrary/model/enums/Genre.java
@@ -0,0 +1,19 @@
+package fmi.project.booklibrary.model.enums;
+
+public enum Genre {
+    ADVENTURE,
+    ACTION,
+    COMIC_BOOK,
+    MYSTERY,
+    FANTASY,
+    HISTORICAL_FICTION,
+    HORROR,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER,
+    BIOGRAPHY,
+    COOK_BOOK,
+    HISTORY,
+    POETRY,
+    OTHER
+}
