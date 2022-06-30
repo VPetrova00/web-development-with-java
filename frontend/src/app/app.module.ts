adfacc1ffef306acc7dfd904c41af6c544dcc78d
Ivana Dragneva <ivanadragneva@gmail.com>
2022-07-01 02:03:45 +0300

    main page of project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoggedOutNavBarComponent } from './logged-out-nav-bar/logged-out-nav-bar.component';
 import { LoggedInNavBarComponent } from './logged-in-nav-bar/logged-in-nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoggedOutNavBarComponent,
-    LoggedInNavBarComponent
+    LoggedInNavBarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
